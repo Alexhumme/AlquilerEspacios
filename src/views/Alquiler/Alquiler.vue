c4c43dd9f4cf6051cc65d5e32a948590270c91a7
kecotes <kecotes@uniguajira.edu.co>
2023-03-06 12:28:29 -0500

    formato pdf

--- src/views/Alquiler/Alquiler.vue
+++ src/views/Alquiler/Alquiler.vue
@@ -63,6 +63,7 @@
   import { Post } from '@/service'
   import { jsPDF } from "jspdf";
   import autoTable from 'jspdf-autotable'
+  import moment from 'moment'
 
   export default {
     props: {
@@ -77,6 +78,11 @@
       }
     },
     methods: {
+      toLocalTime(timeStampObj){
+        const date = (new Date(timeStampObj.seconds)).toLocaleString()
+        
+        return date
+      },
       async printPost(post){
         try {
           console.log("eliminando", post)
@@ -86,40 +92,54 @@
         }
       },
       async toPDF(id) {
-        var vm = this
-        var columns = [
-          {title: "Titulo", dataKey: "Contenido"}
-        ];
-        var doc = new jsPDF('p', 'pt');
-
-        doc.text('FORMATO DE PRÉSTAMO DE ESPACIOS INSTITUCIONALES', 80, 40);
-
         const data = await Post.find(id)
         const result = Object.entries(data)
-        doc.setFontSize(12)
+        const pdfWidth = 21 // ancho de la hoja
+        const pdfHeight = 29.7 // alto de la hoja
+        const marginR = 1.4 // margen a la derecha de la hoja
+        var vm = this
 
+        var doc = new jsPDF('p', 'cm', 'a4');
+
+        const banner = new Image()
+        banner.src = "src/img/liceo-banner.png"
+        banner.onload = () => {
+          // await for the image to be fully loaded            
+        doc.addImage(banner,'png',marginR,0,pdfWidth-marginR*2,3)
+
+        doc.text('FORMATO DE PRÉSTAMO DE ESPACIOS INSTITUCIONALES', pdfWidth/2, 3.5, { align : 'center' });
+        doc.setFontSize(11)
+        doc.setLineWidth(0.04)
+        
+        // 
+
+        doc.rect(marginR, 3.9, pdfWidth-marginR*2, 1, 'S')
         const [espacioSoli] = result.filter(result=> result[0]==="espacioSolicitado")
-        doc.text(`Espacio Solicitado: ${espacioSoli[1]}`, 30, 120);
+        doc.text(`Espacio Solicitado: ${espacioSoli[1]}`, marginR+0.5, 4.5);
+
+        // cuadros de fecha y hora
+
+        doc.rect(marginR, 5.3, ((pdfWidth-marginR*2)/3)-0.5, 1.3)
+        doc.rect(marginR+(((pdfWidth-marginR*2)/3))+0.25, 5.3, ((pdfWidth-marginR*2)/3)-0.5, 1.3)
+        doc.rect(marginR+((((pdfWidth-marginR*2)/3))*2)+0.5, 5.3, ((pdfWidth-marginR*2)/3)-0.5, 1.3)
 
         const [fechaSoli] = result.filter(result=> result[0]==="fechaSolicitud")
-        doc.text(`Fecha Solicitud: ${fechaSoli[1]}`, 30, 150);
+        doc.text(`Fecha Solicitud: ${this.toLocalTime(fechaSoli[1])}`, marginR+0.53, 6);
 
         const [fechaEvento] = result.filter(result=> result[0]==="fechaEvento")
-        doc.text(`Fecha Evento: ${fechaEvento[1]}`, 30, 180);
+        doc.text(`Fecha Evento: ${fechaEvento[1]}`, marginR+(((pdfWidth-marginR*2)/3))+0.53, 6);
+  
+        console.log(moment().get(fechaEvento[1].seconds).toLocaleString())
 
         const [horaInicio] = result.filter(result=> result[0]==="horaInicio")
-        doc.text(`Fecha Evento: ${horaInicio[1]}`, 30, 210);
+        doc.text(`hora inicio: ${this.toLocalTime(horaInicio[1])}`, marginR+((((pdfWidth-marginR*2)/3))*2)+0.7, 5.8);
 
         const [horaFinal] = result.filter(result=> result[0]==="horaFinal")
-        doc.text(`Fecha Evento: ${horaFinal[1]}`, 30, 240);
+        doc.text(`hora fin: ${this.toLocalTime(horaFinal[1])}`, marginR+((((pdfWidth-marginR*2)/3))*2)+0.7, 6.3);
+        
+        //
 
-        const [valAlquiler] = result.filter(result=> result[0]==="valor")
-        doc.text(`Valor Alquiler: ${valAlquiler[1]}`, 30, 270);
-
-        const [contraprestacion] = result.filter(result=> result[0]==="contraprestacion")
-        doc.text(`Contraprestacion: ${contraprestacion[1]}`, 30, 300);
-
-        const datosPersonales = result.filter((result) => {
+        const ArrdatosPersonales = result.filter((result) => {
           return (
             result[0] === "correo" ||
             result[0] === "empresa" ||
@@ -128,16 +148,76 @@
             result[0] === "documento"
           );
         });
+        const datosPersonales = {
+          correo: ArrdatosPersonales.find((dato)=>dato[0]==="correo")[1],
+          empresa: ArrdatosPersonales.find((dato)=>dato[0]==="empresa")[1],
+          telefono: ArrdatosPersonales.find((dato)=>dato[0]==="telefono")[1],
+          solicitante: ArrdatosPersonales.find((dato)=>dato[0]==="solicitante")[1],
+          documento: ArrdatosPersonales.find((dato)=>dato[0]==="documento")[1],
+        }
+        const [valAlquiler] = result.filter(result=> result[0]==="valor")
+        const [contraprestacion] = result.filter(result=> result[0]==="contraprestacion")
         doc.autoTable({
-          head: columns,
-          body: datosPersonales,
-          startY: 340,
+          head: [
+            [{ 
+              content:'Datos del cliente',
+              colSpan: 4,
+              styles: { halign: 'center' }
+           }]
+          ],
+          body: [
+            [
+              { content:'Nombre', styles: { fontStyle: 'bold' }},
+              datosPersonales.solicitante,
+              { content:'Correo', styles: { fontStyle: 'bold' }},
+              datosPersonales.correo
+            ],
+            [
+              { content:'Documento', styles: { fontStyle: 'bold' }},
+              datosPersonales.documento,
+              { content:'Telefono', styles: { fontStyle: 'bold' }},
+              datosPersonales.telefono
+            ],
+            [
+              {
+                content : 'Empresa que representa',
+                styles : { fontStyle: 'bold' },
+                colSpan : 2
+              },
+              {
+                content : datosPersonales.empresa,
+                colSpan : 2
+              },
+
+            ],
+          ],
+          foot: [
+              [
+                { content : 'Valor del alquiler' },
+                { content : "$"+valAlquiler[1], styles : { fontStyle : 'normal' } },
+                { content : 'Contraprestacion' },
+                { content : "$"+contraprestacion[1], styles : { fontStyle : 'normal' } }
+              ],
+          ],
+          startY: 7,
+          tableWidht: pdfWidth-marginR*2,
           // Default for all columns
           styles: { overflow: 'ellipsize', cellWidth: 'wrap' },
           // Override the default above for the text column
           columnStyles: { text: { cellWidth: 'auto' } },
         });
+       
+        //
 
+        doc.rect(marginR, 11.2, pdfWidth-marginR*2, 2.22)
+        doc.text(
+          'Observación: En ningún caso el usuario podrá colocar clavos, tornillos, cintas pegantes, ni pegamento sobre las paredes o muros. Esto para evitar el daño general y deterioro de la pintura. El usuario deberá responder por cualquier daño o pérdida parcial o total, será su responsabilidad devolver el espacio en buen estado y limpio.'
+          , marginR+0.5, 11.7, {
+            maxWidth: pdfWidth-(marginR+0.5)*2
+          });
+
+        
+        //
         const checklist = result.filter((result) => {
           return (
             result[0] === "aire" ||
@@ -145,25 +225,63 @@
             result[0] === "paredes" ||
             result[0] === "pisos" ||
             result[0] === "puertas" ||
-            result[0] === "sanitarios"
+            result[0] === "sanitarios" ||
+            result[0] === "ventanas"
           );
         });
         doc.autoTable({
-          head: columns,
+          head: [
+            [{ content: "Inventario de entrega", colSpan: 3, rowSpan: 1, styles: { halign: 'center' } }],
+            [{ content: 
+              `Se hace entrega del ${ espacioSoli[1] } con los siguientes elementos. Es de recomendar que se debe entregar limpio y con todos los elementos en buen estado`,
+              colSpan: 3, rowSpan: 1, 
+              styles: { 
+                textColor : '#000000',
+                fontStyle : 'normal', 
+                fillColor: '#ffffff'
+              }
+            }],
+            ["Inventario","Estado", "Observaciones"],
+          ],
           body: checklist,
-          startY: doc.lastAutoTable.finalY + 15,
+          startY: 13.8,
           rowPageBreak: 'auto',
           bodyStyles: { valign: 'top' }, 
         });
+        
 
+        //
+        
+        doc.rect(marginR, 22, pdfWidth-marginR*2, 4.3)
+        doc.text("COMPROMISO DE AUTORIZACION DE DATOS PERSONALES", pdfWidth/2, 22.7, { align : 'center' })
+        doc.text(
+          'Me comprometo a cumplir fielmente los siguientes requisitos para el uso del espacio. Autorizo de forma clara, expresa e inequívoca a la institución Educativa Almirante Padilla el tratamiento de mis datos personales, de acuerdo con los parámetros de la ley 1581 de 2012, con la finalidad de que sea autorizado el uso del espacio. En caso de requerir modificar, actualizar o suprimir datos personales debe escribir al correo: rectoriaalpadilla@gmail.com'
+          , marginR+0.5, 23.4, {
+            maxWidth: pdfWidth-(marginR+0.5)*2,
+          }
+        )
+        doc.text("Firma: _________________________________________________", marginR+0.5, 26)
 
-        doc.text("Observación: En ningún caso el usuario podrá colocar clavos, tornillos, cintas pegantes, ni pegamento sobre las paredes o muros. Esto para evitar el daño general y deterioro de la pintura. El usuario deberá responder por cualquier daño o pérdida parcial o total, será su responsabilidad devolver el espacio en buen estado y limpio.", 30, 270);
+        //
 
-        doc.text("Contraprestacion", 30, 300);
-        doc.text(`Contraprestacion: ${contraprestacion[1]}`, 30, 300);
+        doc.rect(marginR, 26.5, pdfWidth-marginR*2, 1)
+        doc.text(
+          `
+Quien entrega: ___________________________________________________________
+# de Cedula y Teléfono: ____________________________________________________
+          `, marginR+0.5, 26.44)
 
+        //
+
+        doc.rect(marginR, 27.5, pdfWidth-marginR*2, 1)
+        doc.text(
+          `
+Quien recibe: ____________________________________________________________
+# de Cedula y Teléfono: ____________________________________________________
+          `, marginR+0.5, 27.44)
 
         doc.save('todos.pdf');
+        };
       }
     },
     setup: () => {
